0d2cff0a44301c711a0741c666b191eb70443474
ojaegyeong <jk96030213@gmail.com>
2019-06-21 23:31:02 +0900

    마무리 작업

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,24 +1,36 @@
 <template>
     <div  v-bind:class="{'scroll_lock': isModal}">
-      <div class="test" v-show="isModal">
-        <v-layout row wrap align-center style="height: 90%">
-          <v-flex >
-            <v-slider
-                    v-model="value"
-                    :vertical="true"
-            ></v-slider>
-            <span class="slide_value" id="value1">{{value}}</span>
-          </v-flex>
-          <v-flex >
-            <v-slider
-                    v-model="value2"
-                    height="'900'"
-                    :vertical="true"
-            ></v-slider>
-            <span class="slide_value" id="value2">{{value2}}</span>
-          </v-flex>
-        </v-layout>
-      </div>
+      <transition name="fade">
+        <div class="test" v-if="isModal" v-on:click="toggleModal">
+          <v-layout row wrap align-center style="height: 90%">
+            <div class="slide_icon" style="left: 22.7%;">
+              <img src="~@/assets/img/icon_1_1.png" style="width: 17px">
+            </div>
+            <div class="slide_icon" style="left: 71.5%; top: 21.5%;">
+              <img src="~@/assets/img/icon_2_1.png">
+            </div>
+            <v-flex>
+              <v-slider
+                v-model="postData.temperature"
+                :vertical="true"
+              ></v-slider>
+              <div class="slide_value" id="value1">
+                <span>{{postData.temperature}}℃</span>
+              </div>
+            </v-flex>
+            <v-flex>
+              <v-slider
+                v-model="postData.water"
+                height="'900'"
+                :vertical="true"
+              ></v-slider>
+              <div class="slide_value" id="value2">
+                <span>{{postData.water}}%</span>
+              </div>
+            </v-flex>
+          </v-layout>
+        </div>
+      </transition>
       <main class="main" v-bind:class="{'blur': isModal}" >
         <svg xmlns="http://www.w3.org/2000/svg"  version="1.1" class="blur-svg">
           <!--    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/2000/xlink" class="blur-svg">-->
@@ -41,22 +53,21 @@
               <div class="img_box temperature">
                 <img src="~@/assets/img/icon_1.png" alt="temperature_icon">
               </div>
-              <div class="text_box" v-on:click="showModal">
+              <div class="text_box" v-on:click="toggleModal">
                 <span class="name">온도</span>
                 <span class="value">{{senseData.temperature}}℃</span>
               </div>
             </article>
             <article class="contents" style="width: 45%">
               <div class="img_box">
-                <img src="~@/assets/img/icon_2.png" alt="temperature_icon2">
+                <img src="~@/assets/img/icon_2.png" alt="temperature_icon2" style="width: 135%">
               </div>
-              <div class="text_box" v-on:click="showModal">
+              <div class="text_box" v-on:click="toggleModal">
                 <span class="name">수분</span>
                 <span class="value">{{senseData.water}}%</span>
               </div>
             </article>
           </section>
-          <button v-on:click="fnConnect" style="width: 30px;">Connection Test</button>
           <section class="condition_box">
             <div class="condition_text_box">
           <span v-on:click="fnChangeStep">
@@ -91,8 +102,10 @@ export default {
   name: 'index',
   data () {
     return {
-      value: 0,
-      value2: 0,
+      postData: {
+        temperature: 0,
+        water: 0
+      },
       isModal: false,
       stepClass: 'step1'
     }
@@ -103,16 +116,29 @@ export default {
       if (val) {
         document.body.className = 'scroll_lock'
       }
+    },
+    postData: {
+      handler (val) {
+        console.log(document.getElementsByClassName('v-slider__track-background')[0])
+        let data = val
+        console.log(data)
+      },
+      deep: true
     }
   },
   computed: {
     senseData: function () {
-      return this.$store.getters['getMqttSenseData']
+      let data = this.$_.cloneDeep(this.$store.getters['getMqttSenseData'])
+      this.postData['temperature'] = data.temperature
+      this.postData['water'] = data.water
+      return data
     }
   },
   methods: {
-    showModal: function () {
-      this.isModal = true
+    toggleModal: function (e) {
+      let target = e.target.className
+      if (target.indexOf('v-slider') > -1) return
+      this.isModal = !this.isModal
     },
     fnConnect: function () {
       // let callBack = this.$AwsIot.mqttClientConnectHandler
@@ -124,6 +150,7 @@ export default {
       // window.updatePublishData()
     },
     fnChangeStep: function () {
+      this.fnConnect()
       if (this.stepClass === 'step1') {
         this.stepClass = 'step2'
       } else if (this.stepClass === 'step2') {
@@ -139,18 +166,18 @@ export default {
 </script>
 <style lang="scss">
   .v-slider--vertical .v-slider__track-container {
-    width: 60px;
+    width: 61px;
     height: 200px;
     .v-slider__track-background {
       // border-radius: 30px;
       border-radius: 15px;
-      /*border-top-left-radius: 30px;*/
-      /*border-top-right-radius: 30px;*/
-      /*border-bottom-left-radius: 30px;*/
-      /*border-bottom-right-radius: 30px;*/
+      /*border-top-left-radius: 15px;*/
+      /*border-top-right-radius: 15px;*/
     }
     .v-slider__track-fill {
       background: rgb(255, 255, 255);
+      /*border-bottom-left-radius: 15px;*/
+      /*border-bottom-right-radius: 15px;*/
       border-radius: 15px;
     }
   }
@@ -175,8 +202,27 @@ export default {
     -ms-flex-direction: column-reverse;
     flex-direction: column-reverse;
     margin-top: 65px;
+    text-align: center;
+    span {
+      font-size: 20px;
+      width: 50px;
+    }
   }
-  /*.v-slider__thumb {*/
-    /*width: 150px;*/
-  /*}*/
+  .slide_icon {
+    position: absolute;
+    top: 21%;
+    left: 50%;
+    width: 25px;
+    img {
+      width: 25px;
+    }
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .27s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+  }
+
 </style>
