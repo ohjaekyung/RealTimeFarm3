99c9e2443b3b035d6b06468ba1a8408230b449f9
jaekyungOh <jk96030213@gmail.com>
2019-06-21 18:20:56 +0900

    sss

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
+import '@/assets/css/main.scss'
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import AwsIot from '@/common/AwsIot.js'
+import _ from 'lodash'
+import VueMeta from 'vue-meta'
+// import Vuetify from 'vuetify'
 
+Vue.use(VueMeta)
+// Vue.use(Vuetify)
 Vue.config.productionTip = false
+Vue.prototype.$AwsIot = AwsIot
+Vue.prototype.$_ = _
 
 new Vue({
+  store,
+  router,
   render: h => h(App),
 }).$mount('#app')
+;
